1bf1e7826192331e4754c3520453df04f9b0600a
Kyriacos Shiarli <kikos1988@gmail.com>
2016-11-08 13:30:43 +0100

    new version includes experimental cvx update. Also a bit more tidy

---
+++ README.md
@@ -0,0 +1,9 @@
+Code to replicate experiments in Inverse Reinforcement Learning from Failure.
+(Shiarlis et al AAMAS 2016)
+
+To replicate experiments run the experiments.py script within the gridworld folder. 
+
+Results will appear in gridworld/results/aamas
+
+
+# IRLF_aamas2016_Replicate
--- gridworld/experiments.py
+++ gridworld/experiments.py
@@ -8,130 +8,14 @@ from tp_data_structures import EmptyObject
 #from plots import *
 from tp_RFeatures import toy_problem_simple,toy_problem_squared
 from tp_plots import plot_results
+from tp_learn_dev import learn_from_failure
 
-def pin_to_zero(vector_to_pin):
-	vector_to_pin = np.array(vector_to_pin)
-	where = [i for i in range(len(vector_to_pin)) if vector_to_pin[i] < 0]
-	vector_to_pin[where]= 0
-	return vector_to_pin
-def inference(model,steps,initial_states,discount = 0.9,z_states=None):
-	policy,log_policy,z_states=fb.backward_sparse(model.transition.sparse,model.reward_f,length = steps,discount = discount,conv=5,z_states = z_states)
-	state_freq,state_action_freq,all_stat = fb.forward_sparse(policy,model.transition.sparse,initial_states,steps,discount=discount) # 30 timesteps
-	return policy,z_states,state_action_freq,all_stat
-def plot_path(path_probs,disc_model,subplot_dims,save_name):
-	fig,axarr = plt.subplots(subplot_dims[0],subplot_dims[1],sharex = False)
-	all_states = disc_model.enum_states
-	steps = len(path_probs[0,:])
-	for i in range(steps):
-		alph = abs(path_probs[:,i]-0.001)
-		axis = axarr[np.floor(i/subplot_dims[1]),i%subplot_dims[1]]
-		green = np.zeros([all_states.shape[0],4]);blue = np.zeros([all_states.shape[0],4])
-		blue[:,:3] = np.array([0,0,1]*all_states.shape[0]).reshape(all_states.shape[0],3) ; blue[:,3] = list(alph)
-		green[:,:3] = np.array([0,1,0]*all_states.shape[0]).reshape(all_states.shape[0],3) ; green[:,3] = list(alph)
-		axis.scatter(disc_model.target[0],disc_model.target[1],color="r",alpha = 0.0001)
-		axis.scatter(all_states[:,0],all_states[:,1],color=blue)
-		axis.scatter(all_states[:,2],all_states[:,3],color=green)
-	fig.savefig(save_name+".png",dpi=80)
-def learn_from_failure(expert1,expert2,apprentice,iterations,steps,initial_states,test_states,failure = "false",initial_bad_states = None):
-	#initialise the lagrange multipliers to 1
-	print "INITIALISED LEARNING. LEARNING FROM FAILURE = ",failure
-	direc ="results/"
-	fn.make_dir(direc)
-	C = 5.0
-	D=.7
-	delta_c = .96
-	disc = expert1.disc
-	
-	a,s,f = expert1.feature_f.shape
-	#experts
-	exp1_policy,ignore,exp1_state_exp,exp1_all = inference(expert1,steps,initial_states,discount =0.9)
-	if initial_bad_states == None:
-		exp2_policy,ignore,exp2_state_exp,exp2_all = inference(expert2,steps,initial_states,discount = 0.9)
-	else:
-		exp2_policy,ignore,exp2_state_exp,exp2_all = inference(expert2,steps,initial_bad_states,discount = 0.9)
-	#print "POLICYY", exp1_policy.shape
-
-	exp1_feature_avg = np.dot(exp1_state_exp.reshape(s*a,order = "F"),expert1.feature_f.reshape(s*a,f,order ="F"))
-	exp2_feature_avg = np.dot(exp2_state_exp.reshape(s*a,order = "F"),expert2.feature_f.reshape(s*a,f,order = "F"))
-
-	e_on_e = eval_value(expert1.w,exp1_policy,expert1,test_states,steps)
-	t_o_t = eval_value(expert2.w,exp2_policy,expert2,test_states,steps)
-	expert_on_taboo = eval_value(expert2.w,exp1_policy,expert2,test_states,steps)
-	z_stat = None
-
-	#initiate results structure
-	results = EmptyObject()
-	results.a_o_e = []
-	results.a_o_t = []
-	results.policy_diff1 = []
-	results.policy_diff2 = []
-	results.e_on_e = e_on_e
-	results.t_o_t = t_o_t
-	results.e_o_t = expert_on_taboo
-	# learning rate
-	rate = 0.08
-	rate2 = 0.08
-	# delay before failure data is includes. Large numbers avoid oscilations
-	delay = 0
-
-	for i in range(iterations):
-		apprentice_policy,z_stat,a_state_exp,a_all = inference(apprentice,steps,initial_states,z_states = None,discount = 0.9)
-		apprentice_feature_avg = np.dot(a_state_exp.reshape(s*a,order = "F"),apprentice.feature_f.reshape(s*a,f,order = "F"))
-		difference_exp1 = exp1_feature_avg - apprentice_feature_avg
-		if initial_bad_states == None:
-			difference_exp2 = exp2_feature_avg - apprentice_feature_avg
-		else:
-			apprentice_policy,z_stat,a_state_exp_bad,a_all = inference(apprentice,steps,initial_bad_states,z_states = None,discount = 0.9)
-			apprentice_feature_avg_bad = np.dot(a_state_exp_bad.reshape(s*a,order = "F"),apprentice.feature_f.reshape(s*a,f,order = "F"))
-			difference_exp2 = apprentice_feature_avg_bad - exp2_feature_avg 
-		if i ==0:
-			difference_random = np.copy(difference_exp2)
-			apprentice_feature_avg_bad_prev = apprentice_feature_avg*0
-		if failure =="L2":
-			#first update the alphas according to their gradient.
-			apprentice.w = fn.pin_to_threshold(apprentice.w + rate*difference_exp1,C,-C)		
-			if i>delay:
-				apprentice.zeta =-difference_exp2
-			#print "ZETAAA",apprentice.zeta
-			#print "-------------------------------------------"s
-		elif failure == "L1":
-			apprentice.w = apprentice.w + rate*difference_exp1
-			#apprentice.zeta = apprentice.zeta + rate2*(difference_exp2+ D*apprentice.zeta)
-			apprentice.zeta = -0.9*difference_exp2
-		elif failure == "false":
-			apprentice.w = apprentice.w + rate*difference_exp1
-		elif failure == "slow":
-			apprentice.w = apprentice.w + rate*difference_exp1	
-			C = C*delta_c
-			if 1./C>D:
-				C = 1/D
-			if i >delay:
-				apprentice.zeta =-difference_exp2/(C)
-		apprentice_feature_avg_bad_prev =apprentice_feature_avg_bad 
-		apprentice.reward_f = apprentice.buildRewardFunction()
-		#evaluation
-		a_on_e = eval_value(expert1.w,apprentice_policy,apprentice,test_states,steps) 
-		a_o_t = eval_value(expert2.w,apprentice_policy,apprentice,test_states,steps) 
-		#if i ==iterations-1:
-		if i <iterations:			
-			print "failure",failure
-			print "Iteration",i
-			print "Aprentice on Expert" ,a_on_e
-			print "Expert on expert",e_on_e
-			print "Apprentice on Taboo",a_o_t
-			print "Taboo on Taboo",t_o_t
-			print "Expert on Taboo",expert_on_taboo
-			print "______________________________________"
-		results.a_o_e.append(a_on_e)
-		results.a_o_t.append(a_o_t)
-		results.policy_diff1.append(np.sum(np.sum(np.absolute(apprentice_policy-exp1_policy)))/(2*disc.tot_states)*100)
-		results.policy_diff2.append(np.sum(np.sum(np.absolute(apprentice_policy-exp2_policy)))/(2*disc.tot_states)*100)
-		if i  == iterations-1:
-			print "Policy Difference",results.policy_diff1[-1]
-			print "Policy Difference",results.policy_diff2[-1]
-	return results
-
-
+# Experiments go as follows:
+# Initialise a discretisation model
+# From that Initialise experts using the rewards required for the experiment.
+# Initialise test and bad states respectively.
+# Make learning runs using different types of apretices i.e that utilise different types of failure (or none)
+# Save results in directory.
 if __name__ == "__main__":
 	def experiment_complementary(expert_feature = toy_problem_simple,apprentice_feature = toy_problem_simple,name = "simple_feature",iterations_per_run=60,steps=15,runs=20):
 		direc = "results/aamas"
@@ -150,11 +34,11 @@ if __name__ == "__main__":
 		for i in range(runs):
 			apprentice = Model(disc_a,"dual_reward", load_saved = True)
 			initial_states = np.random.randint(0,disc.tot_states,20)
-			results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "sign",initial_bad_states = bad_states)
+			results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "L1",initial_bad_states = bad_states)
 			apprentice = Model(disc_a,"dual_reward", load_saved = True)
 			results_normal = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false",initial_bad_states = bad_states)
 			apprentice = Model(disc_a,"dual_reward", load_saved = True)
-			results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "L1",initial_bad_states = bad_states)
+			results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "slow",initial_bad_states = bad_states)
 			results_array.append([results_failure,results_normal,results_slow])
 		fn.pickle_saver(results_array,direc+"/"+name+".pkl")
 
@@ -188,13 +72,13 @@ if __name__ == "__main__":
 				apprentice = Model(disc_a,"dual_reward", load_saved = True)
 				#initial_states = np.random.randint(0,disc.tot_states,5)
 				initial_states = np.random.randint(0,disc.tot_states,size)
-				results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "sign",initial_bad_states = bad_states)
+				results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "L1",initial_bad_states = bad_states)
 				fail[enn,i] = results_failure.e_on_e - results_failure.a_o_e[-1]
 				apprentice = Model(disc_a,"uniform", load_saved = True)
 				results_normal = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false",initial_bad_states = bad_states)
 				normal[enn,i] = results_normal.e_on_e - results_normal.a_o_e[-1]
 				apprentice = Model(disc_a,"dual_reward", load_saved = True)
-				results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "L1",initial_bad_states = bad_states)
+				results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "slow",initial_bad_states = bad_states)
 				slow[enn,i] = results_slow.e_on_e - results_slow.a_o_e[-1]
 				results_array.append([results_failure,results_normal,results_slow])
 		fn.pickle_saver((results_array,fail,normal,slow),direc+"/"+name+".pkl")
@@ -260,8 +144,7 @@ if __name__ == "__main__":
 			results_array.append([results_failure,results_normal,results_slow])
 		fn.pickle_saver(results_array,direc+"/"+name+".pkl")
 
-
-	def experiment_full(expert_feature = toy_problem_simple,apprentice_feature = toy_problem_simple,name = "simple_feature",iterations_per_run=60,steps=15,runs=20):
+	def experiment_cvx_contrasting(expert_feature = toy_problem_simple,apprentice_feature = toy_problem_simple,name = "simple_feature",iterations_per_run=60,steps=15,runs=20):
 		direc = "results/aamas"
 		#initial_states = [disc.quantityToState([0,0,1,2,2]),disc.quantityToState([0,0,3,4,1]),disc.quantityToState([0,1,2,2,2]),disc.quantityToState([0,0,3,2,1])]
 		#test_states =[disc.quantityToState([0,0,2,2,1]),disc.quantityToState([0,0,2,4,2]),disc.quantityToState([0,0,3,1,3]),disc.quantityToState([0,0,3,2,1])]
@@ -270,34 +153,40 @@ if __name__ == "__main__":
 		disc = DiscModel(target = [4,4],boundaries = [4,4],feature = expert_feature)
 		disc_a = DiscModel(target = [4,4],boundaries = [4,4],feature = apprentice_feature)
 
-		expert2 = Model(disc,"obstacle2_reach", load_saved = False)
+		expert2 = Model(disc,"obstacle2", load_saved = False)
 		expert1 = Model(disc,"avoid_reach", load_saved = True)
 		test_states = np.random.randint(0,disc.tot_states,100)
+		bad_states = np.random.randint(0,disc.tot_states,5)	
 		for i in range(runs):
+			apprentice = Model(disc_a,"dual_reward", load_saved = True)
+			initial_states = np.random.randint(0,disc.tot_states,10)
+			results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "L1",initial_bad_states = bad_states)
+			if i ==0: 
+				apprentice.visualise_reward()
 			apprentice = Model(disc_a,"uniform", load_saved = True)
-			initial_states = np.arange(0,disc.tot_states)
-			results_failure = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false")
+			results_normal = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false",initial_bad_states = bad_states)
+			if i ==0: 
+				apprentice.visualise_reward()			
 			apprentice = Model(disc_a,"dual_reward", load_saved = True)
-			results_normal = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false")		
-			apprentice = Model(disc_a,"dual_reward", load_saved = True)
-			results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "false")
+			results_slow = learn_from_failure(expert1,expert2,apprentice,iterations_per_run,steps,initial_states,test_states,failure = "cvx",initial_bad_states = bad_states)
+			if i ==0: 
+				apprentice.visualise_reward()			
 			results_array.append([results_failure,results_normal,results_slow])
 		fn.pickle_saver(results_array,direc+"/"+name+".pkl")
 
-
 		
-	#experiment_overlapping(name = "overlapping",steps =15,iterations_per_run= 40,runs = 1)
-	#experiment_complementary(name = "overlapping",steps =15,iterations_per_run= 40,runs = 1)
-	#experiment_constrasting(name = "contrasting",steps =15,iterations_per_run= 40,runs = 1)
-	#plot_results("cvx_contrasting","results/aamas",1)
-	#plot_results("complementary","results/aamas",2)
-	#plot_results("constrasting","results/aamas",2)
-	experiment_contrasting(name = "contrasting",steps =15,iterations_per_run= 60,runs = 2)
-	experiment_complementary(name = "complementary",steps =15,iterations_per_run=60 ,runs = 2)
-	experiment_overlapping(name = "overlapping",steps =15,iterations_per_run= 60,runs = 2)
+	experiment_overlapping(name = "overlapping",steps =15,iterations_per_run= 40,runs = 2)
+	experiment_complementary(name = "complementary",steps =15,iterations_per_run= 40,runs = 2)
+	experiment_contrasting(name = "contrasting",steps =15,iterations_per_run= 40,runs = 2)
+	experiment_cvx_contrasting(name = "cvx_contrasting",steps =15,iterations_per_run= 40,runs = 2)
+	plot_results("cvx_contrasting","results/aamas",2)
 	plot_results("complementary","results/aamas",2)
 	plot_results("contrasting","results/aamas",2)
-	plot_results("overlapping","results/aamas",2)
+	plot_results("complementary","results/aamas",2)
+	#experiment_complementary(name = "complementary",steps =15,iterations_per_run=60 ,runs = 2)
+	#xperiment_overlapping(name = "overlapping",steps =15,iterations_per_run= 60,runs = 2)
+	#plot_results("complementary","results/aamas",2)
+	#plot_results("overlapping","results/aamas",2)
 
 
 
